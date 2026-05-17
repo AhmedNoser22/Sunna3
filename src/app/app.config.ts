f82b2f224037fc41467ff052b4e10a7cf6bccb33
Ahmed Noser <ahmednoseer2004@gmail.com>
2026-05-17 05:10:47 +0300

    google

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideZoneChangeDetection } from '@angular/core';
 import { provideBrowserGlobalErrorListeners } from '@angular/core';
 import { routes } from './app.routes';
 import { auth } from './interceptors/auth';
+import { SocialAuthServiceConfig, GoogleLoginProvider } from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +16,18 @@ export const appConfig: ApplicationConfig = {
       withFetch(),
       withInterceptors([auth])
     ),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('797767670488-acbfjsrhclh93c7auo6jp44rgsq38gt0.apps.googleusercontent.com')
+          }
+        ],
+        onError: (err: any) => console.error(err)
+      } as SocialAuthServiceConfig
+    }
   ],
 };
